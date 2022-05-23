ca650eb9fd65b7dc3f613fbd9230abaf3cd62087
Hector <hectorpose@gmail.com>
2022-05-23 16:43:21 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from operaciones import calculos
+a = 4
+b = 2
+
+print(f" Suma: {calculos.suma(a,b)}, Resta: {calculos.resta(a,b)}, Multiplicacion: {calculos.multiplicacion(a,b)}, Division: {calculos.division(4,2)}")
---
+++ operaciones/calculos.py
@@ -0,0 +1,16 @@
+
+def suma(a,b):
+    c = a+b
+    return c
+
+def resta(a,b):
+    c = a-b
+    return c
+
+def multiplicacion(a,b):
+    c = a*b
+    return c
+
+def division(a,b):
+    c = a/b
+    return c
